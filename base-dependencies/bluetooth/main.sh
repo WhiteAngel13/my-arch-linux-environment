56e0eb631ac002286667a4dd89adb341610e57c5
whiteangel13 <edmilsonsw22@gmail.com>
2023-05-29 18:12:33 -0400

    feat: implements base_dependencies_bluetooth

---
+++ base-dependencies/bluetooth/main.sh
@@ -0,0 +1,14 @@
+base_dependencies_bluetooth () {
+  # Install Bluez
+  echo "Checking if Bluez is installed"
+  if ! command -v bluetoothctl &> /dev/null
+  then
+    echo "Attempting to install Bluez"
+    paru -S bluez
+    paru -S bluez-utils
+    systemctl enable bluetooth.service
+    systemctl start bluetooth.service
+  else
+    echo "Bluez is already installed"
+  fi
+}
---
+++ base-dependencies/main.sh
@@ -0,0 +1,9 @@
+source "${BASE_DIR}/base-dependencies/git/main.sh"
+source "${BASE_DIR}/base-dependencies/paru/main.sh"
+source "${BASE_DIR}/base-dependencies/bluetooth/main.sh"
+
+base_dependencies(){
+  base_dependencies_git
+  base_dependencies_paru
+  base_dependencies_bluetooth
+}
