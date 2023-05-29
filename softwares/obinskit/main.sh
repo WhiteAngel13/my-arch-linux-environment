2c3f49f8ffd76f9e7e2c9907a191c45f2861bcab
whiteangel13 <edmilsonsw22@gmail.com>
2023-05-29 18:24:00 -0400

    feat: implements software_obinskit

---
+++ softwares/obinskit/main.sh
@@ -0,0 +1,11 @@
+software_obinskit(){
+  # Install Obinskit to my AnnePro2
+  echo "Checking if Obinskit is installed..."
+  if ! command -v obinskit &> /dev/null
+  then
+    echo "Attempting to install Obinskit..."
+    paru -S obinskit
+  else
+    echo "Obinskit is already installed"
+  fi
+}
