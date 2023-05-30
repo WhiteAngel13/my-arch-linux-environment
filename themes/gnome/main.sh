7f0c5867c7041790a8e53d0703ca717763989d04
whiteangel13 <edmilsonsw22@gmail.com>
2023-05-29 22:25:28 -0400

    feat: implements anha

--- themes/gnome/main.sh
+++ themes/gnome/main.sh
@@ -7,10 +7,14 @@ themes_gnome(){
   fi
 
   # Check if background exists at $HOME/Pictures/backgrounds/anha.jpg
-
-  if [ ! -f "$HOME/Pictures/backgrounds/anha.jpg" ]; then
+  if [ ! -f "$HOME/Pictures/backgrounds/anha.png" ]; then
     echo "Downloading background..."
     URL="https://raw.githubusercontent.com/whiteangel13/my-arch-linux-environment/master/themes/gnome/anha.png"
+    
+    mkdir -p "$HOME/Pictures/backgrounds"
     curl -o "$HOME/Pictures/backgrounds/anha.png" "$URL"
+
+    echo "Setting background..."
+    gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/backgrounds/anha.png"
   fi
 }
